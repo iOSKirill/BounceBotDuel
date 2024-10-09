5d88a1c6fa9efc65bd8127b70d96a5a4b009f428
Kirill Manuilenko <m.kirill4163@gmail.com>
2024-10-09 19:22:32 +0300

    Initial commit #2

---
+++ BounceBotDuel/BounceBotDuel/BounceBotDuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BounceBotDuelApp.swift
+//  BounceBotDuel
+//
+//  Created by Kirill Manuilenko on 9.10.24.
+//
+
+import SwiftUI
+
+@main
+struct BounceBotDuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
